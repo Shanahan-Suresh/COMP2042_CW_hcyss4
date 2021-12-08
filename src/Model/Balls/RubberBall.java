4e51478c1ea0b80e1ac0405d41e8808b2b589f45
Shanahan-Suresh <SHANAHAN.SURESH1@GMAIL.COM>
2021-12-09 07:31:05 +0800

    Modify RubberBall methods according to its parent

--- src/Model/Balls/RubberBall.java
+++ src/Model/Balls/RubberBall.java
@@ -1,6 +1,6 @@
 /*
  *  Brick Destroy - A simple Arcade video game
- *   Copyright (C) 2017  Filippo Ranza
+ *   Copyright (C) 2021  Shanahan Suresh
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,23 +23,21 @@ import java.awt.geom.Point2D;
 
 public class RubberBall extends Ball {
 
-
     private static final int DEF_RADIUS = 10;
     private static final Color DEF_INNER_COLOR = new Color(255, 219, 88);
     private static final Color DEF_BORDER_COLOR = DEF_INNER_COLOR.darker().darker();
-
-
+    
     public RubberBall(Point2D center){
-        super(center,DEF_RADIUS,DEF_RADIUS,DEF_INNER_COLOR,DEF_BORDER_COLOR);
+        super(center,DEF_RADIUS,DEF_INNER_COLOR,DEF_BORDER_COLOR);
     }
 
 
     @Override
-    protected Shape makeBall(Point2D center, int radiusA, int radiusB) {
+    protected Shape makeBall(Point2D center, int radius) {
 
-        double x = center.getX() - (radiusA / 2);
-        double y = center.getY() - (radiusB / 2);
+        double x = center.getX() - (radius / 2);
+        double y = center.getY() - (radius / 2);
 
-        return new Ellipse2D.Double(x,y,radiusA,radiusB);
+        return new Ellipse2D.Double(x,y,radius, radius);
     }
 }
