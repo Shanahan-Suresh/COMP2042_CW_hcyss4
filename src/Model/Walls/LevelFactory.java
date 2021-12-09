f2272a4a3a162cf2c85acffc574cdb40160f938e
Shanahan-Suresh <SHANAHAN.SURESH1@GMAIL.COM>
2021-12-09 11:22:12 +0800

    Add Walls subpackage and empty LevelFactory class
    New branch created to implement a Factory design pattern for level
    creation.

---
+++ src/Model/Walls/LevelFactory.java
@@ -0,0 +1,5 @@
+package Model.Walls;
+
+public class LevelFactory {
+    
+}
--- src/Model/Walls/Wall.java
+++ src/Model/Walls/Wall.java
@@ -15,10 +15,11 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package Model;
+package Model.Walls;
 
 import Model.Balls.*;
 import Model.Bricks.*;
+import Model.Player;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
