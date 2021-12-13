35f0332fd22eba1f911b184e95fa8aaa3d929567
Shanahan-Suresh <SHANAHAN.SURESH1@GMAIL.COM>
2021-12-13 14:40:15 +0800

    Moved audio files into Resources folder

--- src/Model/Sounds/CrackSound.java
+++ src/Model/Sounds/CrackSound.java
@@ -3,7 +3,7 @@ package Model.Sounds;
 public class CrackSound extends Sound {
 
     public CrackSound() {
-        this.sound = "src/Model/Sounds/CrackBrick.wav";
+        this.sound = "src/Model/Resources/CrackBrick.wav";
         setFile(sound);
         play();
     }
--- src/Model/Sounds/HitSound.java
+++ src/Model/Sounds/HitSound.java
@@ -3,7 +3,7 @@ package Model.Sounds;
 public class HitSound extends Sound {
 
     public HitSound() {
-        this.sound  = "src/Model/Sounds/HitBrick.wav";
+        this.sound  = "src/Model/Resources/HitBrick.wav";
         setFile(sound);
         play();
     }
